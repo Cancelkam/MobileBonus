8ae20e4be38ab4be8d4f48d1f6cbdcf5910712d2
Cancelkam <Cancel.kam@gmail.com>
2020-02-26 11:26:07 +0300

    bonus count added v1.1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int clientBalance = 100;
-        int balanceRefill = 3000;
-        int step = 100;
+        int balanceRefill = 100;
+        int step = 500;
         int bonus = balanceRefill / step;
         if (balanceRefill > 1000) {
             System.out.println(bonus);
+        }
+        else {
+           bonus = 0 ;
+           System.out.println(bonus);
         }
     }
 }
